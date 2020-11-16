95836a35cc1d53bde10cccc8a85a62a9a68f00fa
suha-dewan <74549674+suha-dewan@users.noreply.github.com>
2020-11-16 19:52:34 +0000

    Add files via upload

---
+++ src/covidtracker/LinkedList.java
@@ -0,0 +1,473 @@
+package prj5;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedList<T> implements LList<T> {
+
+    public static class Node<D> {
+
+        // The data element stored in the node.
+        private D data;
+
+        // The next node in the sequence.
+        private Node<D> next;
+
+        /**
+         * Creates a new node with the given data
+         *
+         * @param d
+         *            the data to put inside the node
+         */
+        public Node(D d) {
+            data = d;
+        }
+
+
+        /**
+         * Sets the node after this node
+         *
+         * @param n
+         *            the node after this one
+         */
+        public void setNext(Node<D> n) {
+            next = n;
+        }
+
+
+        /**
+         * Gets the next node
+         *
+         * @return the next node
+         */
+        public Node<D> next() {
+            return next;
+        }
+
+
+        /**
+         * Gets the data in the node
+         *
+         * @return the data in the node
+         */
+        public D getData() {
+            return data;
+        }
+    }
+
+    private Node<T> head;
+
+    // the size of the linked list
+    private int size;
+
+    /**
+     * Creates a new LinkedList object
+     */
+    public LinkedList() {
+        head = null;
+        size = 0;
+
+    }
+
+
+    /**
+     * Gets the number of elements in the list
+     *
+     * @return the number of elements
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    /**
+     * Adds the object to the position in the list
+     *
+     * @precondition obj cannot be null
+     * @param index
+     *            where to add the object
+     * @param obj
+     *            the object to add
+     * @throws IndexOutOfBoundsException
+     *             if index is less than zero or greater than size
+     * @throws IllegalArgumentException
+     *             if obj is null
+     */
+    @Override
+    public void add(int index, T obj) {
+        if (obj == null) {
+            throw new IllegalArgumentException("Object is null");
+        }
+
+        if ((index < 0) || (index > size())) {
+            throw new IndexOutOfBoundsException("Index is out of bounds");
+        }
+
+        Node<T> current = head;
+
+        if (isEmpty()) {
+            head = new Node<T>(obj);
+        }
+
+        else {
+            if (index == 0) {
+                Node<T> newNode = new Node<T>(obj);
+                newNode.setNext(head);
+                head = newNode;
+            }
+            else {
+
+                for (int i = 0; i < index - 1; i++) {
+                    current = current.next;
+                }
+                Node<T> nextNext = current.next;
+                Node<T> newNode = new Node<T>(obj);
+                current.setNext(newNode);
+                newNode.setNext(nextNext);
+
+            }
+            current = current.next();
+
+        }
+
+        size++;
+    }
+
+
+    /**
+     * Adds the object to the end of the list.
+     *
+     * @precondition obj cannot be null
+     * @param obj
+     *            the object to add
+     * @throws IllegalArgumentException
+     *             if obj is null
+     */
+    @Override
+    public void add(T obj) {
+        if (obj == null) {
+            throw new IllegalArgumentException("Object is null");
+        }
+
+        Node<T> current = head;
+
+        if (isEmpty()) {
+            head = new Node<T>(obj);
+        }
+
+        else {
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.setNext(new Node<T>(obj));
+        }
+        size++;
+    }
+
+
+    /**
+     * Checks if the array is empty
+     *
+     * @return true if the array is empty
+     */
+    @Override
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+
+    /**
+     * Removes the first instance of the given object from the list
+     *
+     * @param obj
+     *            the object to remove
+     * @return true if successful
+     */
+    @Override
+    public boolean remove(T obj) {
+        Node<T> current = head;
+
+        if ((head != null) && (obj.equals(current.data))) {
+            head = head.next;
+            size--;
+            return true;
+        }
+
+        while (size() >= 2 && (current.next != null)) {
+            if ((obj.equals(current.next.data))) {
+                if (current.next.next != null) {
+                    current.setNext(current.next.next);
+                }
+                else {
+                    current.setNext(null);
+                }
+                size--;
+                return true;
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Removes the object at the given position
+     *
+     * @param index
+     *            the position of the object
+     * @return true if the removal was successful
+     * @throws IndexOutOfBoundsException
+     *             if there is not an element at the index
+     */
+    @Override
+    public boolean remove(int index) {
+        if (index < 0 || index > size || isEmpty()) {
+            throw new IndexOutOfBoundsException();
+        }
+        else if (size == 1) {
+            clear();
+            return true;
+        }
+        else if (index == 0) {
+            head = head.next;
+            size--;
+            return true;
+        }
+        else {
+            Node<T> current = head;
+            for (int i = 1; i < index; i++) {
+                current = current.next;
+            }
+
+            Node<T> newNext = current.next.next;
+            current.next = newNext;
+
+            size--;
+
+            return true;
+        }
+
+    }
+
+
+    /**
+     * Gets the object at the given position
+     *
+     * @param index
+     *            where the object is located
+     * @return The object at the given position
+     * @throws IndexOutOfBoundsException
+     *             if no node at the given index
+     */
+    @Override
+    public T get(int index) {
+        Node<T> current = head;
+        int currentIndex = 0;
+        T data = null;
+        while (current != null) {
+            if (currentIndex == index) {
+                data = current.data;
+            }
+            currentIndex++;
+            current = current.next;
+        }
+
+        if (data == null) {
+            throw new IndexOutOfBoundsException("Index exceeds the size.");
+        }
+        return data;
+    }
+
+
+    /**
+     * Checks if the list contains the given object
+     *
+     * @param obj
+     *            the object to check for
+     * @return true if it contains the object
+     */
+    @Override
+    public boolean contains(T obj) {
+        Node<T> current = head;
+        while (current != null) {
+            if (obj.equals(current.data)) {
+                return true;
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Removes all of the elements from the list
+     */
+    @Override
+    public void clear() {
+        size = 0;
+        head = null;
+
+    }
+
+
+    /**
+     * Gets the last time the given object is in the list
+     *
+     * @param obj
+     *            the object to look for
+     * @return the last position of it. -1 If it is not in the list
+     */
+    @Override
+    public int lastIndexOf(T obj) {
+        int lastIndex = -1;
+        Node<T> current = head;
+        int currentIndex = 0;
+        while (current != null) {
+            if (obj.equals(current.data)) {
+                lastIndex = currentIndex;
+            }
+            currentIndex++;
+            current = current.next;
+
+        }
+        return lastIndex;
+    }
+
+
+    /**
+     * Returns a string representation of the list If a list contains A, B, and
+     * C, the following should be returned "{A, B, C}" (Without the quotations)
+     *
+     * @return a string representing the list
+     */
+    @Override
+    public String toString() {
+        String result = "{";
+
+        Node<T> current = head;
+        while (current != null) {
+            result += "" + current.data;
+            current = current.next;
+            if (current != null) {
+                result += ", ";
+            }
+        }
+        result += "}";
+        return result;
+    }
+
+
+    /**
+     * Returns an array representation of the list If a list contains A, B, and
+     * C, the following should be returned {A, B, C}, If a list
+     * contains A, B, C, and C the following should be returned {A, B, C, C}
+     *
+     * @return an array representing the list
+     */
+    public Object[] toArray() {
+
+        Object[] array = new Object[this.size()];
+
+        Node<T> current = head;
+        int count = 0;
+        while (current != null) {
+            array[count] = current.getData();
+            current = current.next;
+            count++;
+        }
+
+        return array;
+    }
+
+
+    /**
+     * Returns true if both lists have the exact same contents
+     * in the exact same order
+     *
+     * @return a boolean of whether two lists have the same contents,
+     *         item per item and in the same order
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() == obj.getClass()) {
+            @SuppressWarnings("unchecked")
+            LinkedList<T> other = ((LinkedList<T>)obj);
+            if (other.size() == this.size()) {
+                Node<T> current = head;
+                Node<T> otherCurrent = other.head;
+                while (current != null) {
+                    if (!current.getData().equals(otherCurrent.getData())) {
+                        return false;
+                    }
+                    current = current.next();
+                    otherCurrent = otherCurrent.next();
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Iterator method creates Iterator object
+     *
+     * @return new Iterator object
+     */
+    public Iterator<T> iterator() {
+        return new DLListIterator<T>();
+    }
+
+    private class DLListIterator<A> implements Iterator<T> {
+
+        private int index;
+
+        /**
+         * Creates a new DLListIterator
+         */
+        public DLListIterator() {
+            index = 0;
+        }
+
+
+        /**
+         * Returns true if this list iterator has more elements when traversing
+         * the list in the reverse direction.
+         *
+         * @return true if there are more elements in the list
+         */
+        @Override
+        public boolean hasNext() {
+            return index < size();
+        }
+
+
+        /**
+         * Gets the next value in the list
+         *
+         * @return the next value
+         * @throws NoSuchElementException
+         *             if there are no nodes left in the list
+         */
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            index++;
+            return get(index - 1);
+        }
+
+    }
+
+}
